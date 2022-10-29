eab76ca2fd54a8493b0575f09b3470ec5edfc38d
majiaao <jiaao.mario@gmail.com>
2022-10-30 03:28:35 +0800

    feat:Object Key Paths https://github.com/type-challenges/type-challenges/blob/main/questions/07258-hard-object-key-paths/README.md

---
+++ src/hard/object-key-paths.ts
@@ -0,0 +1,68 @@
+/*
+ * @Description: Object Key Paths
+ * @href: https://github.com/type-challenges/type-challenges/blob/main/questions/07258-hard-object-key-paths/README.md
+ * @Author: mario.ma
+ * @Date: 2022-10-30 00:40:43
+ * @tags: ['object-keys', 'need-review']
+ */
+
+// Get all possible paths that could be called by _.get (a lodash function) to get the value of an object
+
+/* _____________ Your Code Here _____________ */
+
+type GenNode<K extends string | number,IsRoot extends boolean> = IsRoot extends true? `${K}`: `.${K}` | (K extends number? `[${K}]` | `.[${K}]`:never)
+
+type ObjectKeyPaths<
+  T extends object,
+  IsRoot extends boolean = true,
+  K extends keyof T = keyof T
+> = 
+K extends string | number ?
+  GenNode<K,IsRoot> | (T[K] extends object? `${GenNode<K,IsRoot>}${ObjectKeyPaths<T[K],false>}`:never)
+  :never;
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect, ExpectExtends } from '@type-challenges/utils'
+
+const ref = {
+  count: 1,
+  person: {
+    name: 'cattchen',
+    age: 22,
+    books: ['book1', 'book2'],
+    pets: [
+      {
+        type: 'cat',
+      },
+    ],
+  },
+}
+
+type cases = [
+  Expect<Equal<ObjectKeyPaths<{ name: string; age: number }>, 'name' | 'age'>>,
+  Expect<
+  Equal<
+  ObjectKeyPaths<{
+    refCount: number
+    person: { name: string; age: number }
+  }>,
+  'refCount' | 'person' | 'person.name' | 'person.age'
+  >
+  >,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'count'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.name'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.age'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.pets'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books.0'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books.1'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books[0]'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.books.[0]'>>,
+  Expect<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.pets.0.type'>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'notExist'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.notExist'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.name.'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, '.person.name'>, false>>,
+  Expect<Equal<ExpectExtends<ObjectKeyPaths<typeof ref>, 'person.pets.[0]type'>, false>>,
+]
