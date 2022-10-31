9d9445220bcbda884d47572c29c9b7e1488fdd6d
majiaao <jiaao.mario@gmail.com>
2022-11-01 00:38:36 +0800

    feat: add Binary to Decimal https://github.com/type-challenges/type-challenges/blob/main/questions/06141-hard-binary-to-decimal/README.md

--- src/hard/binary-to-decimal.ts
+++ src/hard/binary-to-decimal.ts
@@ -3,24 +3,23 @@
  * @href: https://github.com/type-challenges/type-challenges/blob/main/questions/06141-hard-binary-to-decimal/README.md
  * @Author: mario.ma
  * @Date: 2022-10-31 12:38:27
- * @tags: ['math']
+ * @tags: ['math', 'need-review']
  */
 
 // Implement BinaryToDecimal<S> which takes an exact string type S consisting 0 and 1 and returns an exact number type corresponding with S when S is regarded as a binary. You can assume that the length of S is equal to or less than 8 and S is not empty.
 
 /* _____________ Your Code Here _____________ */
 
-type ToTuple<T extends number, Result extends unknown[] = []> = T extends Result['length']
+type ToTuple<T extends number, Result extends unknown[] = []> = Result['length'] extends T
                                                                     ? Result
                                                                     : ToTuple<T, [...Result, unknown]>;
 
-type Trans<N extends number, Set extends number, Result extends unknown[] = []> = N extends 0
-                                                                                        ? Result
-                                                                                        : 
+type Copy<T extends unknown[] = []> = [...T, ...T];
 
-type BinaryToDecimal<S extends string, Result extends unknown[] = []> = S extends `${infer R}${infer U}`
-                                            ?
-                                            :
+
+type BinaryToDecimal<S extends string, Result extends unknown[] = []> = S extends `${infer R extends number}${infer U}`
+                                            ? BinaryToDecimal<U, [...Copy<Result>, ...ToTuple<R>]>
+                                            : Result['length'];
 
 
 /* _____________ Test Cases _____________ */
